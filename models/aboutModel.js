e08ed67b06349ee272a3c832cebbbe881f7ff893
Rinzin <justlittletoxic@gmail.com>
2025-06-05 10:58:27 +0600

    aboutModel.js

--- models/aboutModel.js
+++ models/aboutModel.js
@@ -2,8 +2,8 @@ const teamMembers = [
   { name: 'Rinzin Dema', role: 'Backend Developer', image: 'rinzin.png' },
   { name: 'Rinchen Dorji', role: 'UIUX Designer', image: 'rinchen.png' },
   { name: 'Yeshi Choden', role: 'Frontend Developer', image: 'yeshey.png' },
-  { name: 'Manita Gurung', role: 'Frontend Developer', image: 'manita.png' },
-  { name: 'Pema Choden', role: 'Frontend Developer', image: 'pc.png' },
+  { name: 'Manita Gurung', role: 'Frontend Developer', image: 'Manita.png' },
+  { name: 'Pema Choden', role: 'Frontend Developer', image: 'PC.png' },
   { name: 'Pema Wangdi', role: 'Frontend Developer', image: 'pw.png' }
 ];
 
